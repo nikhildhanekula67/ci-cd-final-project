3784d859ee763c747117e59c9c817ab4754e490e
nikhildhanekula67 <nikhildhanekula67@gmail.com>
2025-05-30 16:26:12 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -92,8 +92,8 @@ def update_counters(name):
     """Updates a counter"""
     app.logger.info("Request to Update counter: %s...", name)
 
-    if name not in COUNTER: return abort(status.HTTP_404_NOT_FOUND, 
-                           f"Counter {name} does not exist")
+    if name not in COUNTER: 
+        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
 
     COUNTER[name] += 1
 
